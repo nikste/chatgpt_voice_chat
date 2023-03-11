b81505b3225bf7ab27b978daea4fa2f961a608cc
Nikolaas Steenbergen <nikolaas.steenbergen@gmail.com>
2023-03-11 16:04:07 +0100

    Add Qt6 with button text change and recording in Qrunnable

--- voice_chat.py
+++ voice_chat.py
@@ -1,75 +1,130 @@
-# This is a sample Python script.
-from time import sleep
 
-from tkinter import *
+import sys
+
+import torch
+from PyQt6.QtCore import pyqtSlot, pyqtSignal, QRunnable, QObject, QThreadPool
+from PyQt6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget
 import openai
+from pydub.playback import play
+from pydub import AudioSegment
+import pyaudio
 
 with open("openai_api_key", 'r') as f:
-    api_key = f.read()[:-1] # remove new line
+    api_key = f.read()[:-1]  # remove new line
 openai.api_key = api_key
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-# def voice_rec():
+class WorkerSignals(QObject):
+    '''
+    Defines the signals available from a running worker thread.
+
+    Supported signals are:
+
+    finished
+        No data
+
+    error
+        tuple (exctype, value, traceback.format_exc() )
+
+    result
+        object data returned from processing, anything
+
+    progress
+        int indicating % progress
+
+    '''
+    finished = pyqtSignal()
+    error = pyqtSignal(tuple)
+    result = pyqtSignal(object)
+    progress = pyqtSignal(int)
 
 
-# Import the necessary modules.
-import tkinter
-import tkinter as tk
-import tkinter.messagebox
-import pyaudio
-import wave
-from pydub import AudioSegment
-import os
-import torch
-from pydub.playback import play
+class RecordingWorker(QRunnable):
+    '''
+    Worker thread
 
+    Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
 
-class RecAUD:
+    :param callback: The function callback to run on this worker thread. Supplied args and
+                     kwargs will be passed through to the runner.
+    :type callback: function
+    :param args: Arguments to pass to the callback function
+    :param kwargs: Keywords to pass to the callback function
 
-    def __init__(self, chunk=3024, frmat=pyaudio.paInt16, channels=2, rate=44100, py=pyaudio.PyAudio()):
+    '''
 
-        # Start Tkinter and set Title
-        self.main = tkinter.Tk()
-        self.collections = []
-        self.main.geometry('300x200')
-        self.main.title('Recorder')
-        self.CHUNK = chunk
-        self.FORMAT = frmat
+    def __init__(self, format, channels, rate, chunk, verbose=False):
+        self.verbose = verbose
+        if self.verbose:
+            print("initializing recorder!")
+        super(RecordingWorker, self).__init__()
+        self.FORMAT = format
         self.CHANNELS = channels
         self.RATE = rate
-        self.p = py
+        self.CHUNK = chunk
+        self.st = 0
+        self.p = pyaudio.PyAudio()
         self.frames = []
+
+    @pyqtSlot()
+    def run(self):
+        '''
+        Initialise the runner function with passed args, kwargs.
+        '''
+        if self.verbose:
+            print("starting to record!")
         self.st = 1
-        self.stream = self.p.open(format=self.FORMAT, channels=self.CHANNELS, rate=self.RATE, input=True,
-                                  frames_per_buffer=self.CHUNK)
+        # Retrieve args/kwargs here; and fire processing using them
+        self.frames = []
+        stream = self.p.open(format=self.FORMAT, channels=self.CHANNELS,
+                             rate=self.RATE, input=True,
+                             frames_per_buffer=self.CHUNK)
+        c = 0
+        while self.st == 1:
+            data = stream.read(self.CHUNK)
+            self.frames.append(data)
+            if self.verbose:
+                if c % 10 == 1:
+                    print(c, "* recording")
+            c += 1
+        sound = AudioSegment(
+            # raw audio data (bytes)
+            data=b''.join(self.frames),
+            # 2 byte (16 bit) samples
+            sample_width=self.CHANNELS,  # 2,
+            # 44.1 kHz frame rate
+            frame_rate=self.RATE,  # 44100,
+            # stereo
+            channels=2
+        )
+        if self.verbose:
+            print("finished recording!")
+        # TODO(nik): remove saving to disk, this should stay in memory
+        sound.export("test_recording.mp3", format="mp3")
 
-        # Set Frames
-        self.buttons = tkinter.Frame(self.main, padx=120, pady=20)
 
-        # Pack Frame
-        self.buttons.pack(fill=tk.BOTH)
+class Window(QWidget):
+    def __init__(self, parent=None, verbose=False):
+        super().__init__(parent)
+        self.verbose = verbose
+        self.st = 0
+        self.CHUNK = 3024
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = 2
+        self.RATE = 44100
+        self.threadpool = QThreadPool()
+        self.setup()
 
-        # Start and Stop buttons
-        self.strt_rec = tkinter.Button(self.buttons, width=10, padx=10, pady=5, text='Start Recording',
-                                       command=lambda: self.start_record())
-        self.strt_rec.grid(row=0, column=0, padx=50, pady=5)
-        self.stop_rec = tkinter.Button(self.buttons, width=10, padx=10, pady=5, text='Stop Recording',
-                                       command=lambda: self.stop())
-        self.stop_rec.grid(row=1, column=0, columnspan=1, padx=50, pady=5)
+        # init Gui
+        self.button = QPushButton("Record !")
+        self.button.setFixedSize(120, 60)
+        self.button.clicked.connect(self.record)
+        layout = QVBoxLayout()
+        layout.addWidget(self.button)
+        self.setLayout(layout)
 
-        self.messages = [
-            {"role": "system", "content": "You are an helpful asistant, who wont mention that its created by openai."},
-            {"role": "user", "content": "Who are you?"}]
-        # r = openai.ChatCompletion.create(
-        #       model="gpt-3.5-turbo",
-        #       messages=self.messages
-        #     )
-        # for c in r['choices']:
-        #     self.messages.append(c['message'])
-        message = {'role': 'assistant',
-                   "content": 'I am an Artificial Intelligence, here to assist you with any questions or tasks you may have. How can I help you today?'}
+    def setup(self):
+        # setup voice syntheszer
 
         print("loading tacotron2")
         self.tacotron2 = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_tacotron2', model_math='fp16')
@@ -84,9 +139,14 @@ class RecAUD:
 
         self.tacotron2_utils = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_tts_utils')
 
+        self.messages = [
+            {"role": "system", "content": "You are an helpful asistant, who wont mention that its created by openai."},
+            {"role": "user", "content": "Who are you?"}]
+
+        message = {'role': 'assistant',
+                   "content": 'I am an Artificial Intelligence, here to assist you with any questions or tasks you may have. How can I help you today?'}
         self.say(message)
         self.messages.append(message)
-        tkinter.mainloop()
 
     def say(self, message):
         text_long = message['content']
@@ -109,83 +169,65 @@ class RecAUD:
                 channels=1
             )
             play(audio_segment)
-        # sound = AudioSegment(
-        #     # raw audio data (bytes)
-        #     data=b''.join(self.frames),
-        #     # 2 byte (16 bit) samples
-        #     sample_width=self.CHANNELS,  # 2,
-        #     # 44.1 kHz frame rate
-        #     frame_rate=rate,  # 44100,
-        #     # stereo
-        #     channels=2
-        # )
-        # play(sound)
 
-    def start_record(self):
-        self.st = 1
-        self.frames = []
-        stream = self.p.open(format=self.FORMAT, channels=self.CHANNELS, rate=self.RATE, input=True,
-                             frames_per_buffer=self.CHUNK)
-        c = 0
-        while self.st == 1:
-            data = stream.read(self.CHUNK)
-            self.frames.append(data)
-            if c % 10 == 1:
-                print(c, "* recording")
-            self.main.update()
-            c += 1
+    def transcribe_audio(self, filename="test_recording.mp3"):
+        audio_file = open(filename, "rb")
+        if self.verbose:
+            print("sending transcription request")
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
 
-        # stream.close()
-        #
-        # wf = wave.open('test_recording.wav', 'wb')
-        # wf.setnchannels(self.CHANNELS)
-        # wf.setsampwidth(self.p.get_sample_size(self.FORMAT))
-        # wf.setframerate(self.RATE)
-        # wf.writeframes(b''.join(self.frames))
-        # Advanced usage, if you have raw audio data:
-        sound = AudioSegment(
-            # raw audio data (bytes)
-            data=b''.join(self.frames),
-            # 2 byte (16 bit) samples
-            sample_width=self.CHANNELS,  # 2,
-            # 44.1 kHz frame rate
-            frame_rate=self.RATE,  # 44100,
-            # stereo
-            channels=2
+        print("transcribed:", transcript['text'])
+
+        self.messages.append({"role": "user", "content": transcript['text']})
+
+    def ask_chatgpt(self):
+        if self.verbose:
+            print("sending request to gpt-3")
+        r = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=self.messages
         )
+        if self.verbose:
+            print("answer received")
+        for c in r['choices']:
+            self.messages.append(c['message'])
 
-        sound.export("test_recording.mp3", format="mp3")
-        # wf.close()
+    def stop_recording(self):
+        # stop recording
+        self.recorder.st = 0
+
+        self.button.disconnect()
+        self.button.clicked.connect(self.record)
+        self.button.setText("Record !")
+        # force update gui
+        QApplication.processEvents()
+
+        # Execute
+        all_finished_correctly = self.threadpool.waitForDone()
+        if self.verbose:
+            print("all threads exited gracefully:", all_finished_correctly)
+
         self.transcribe_audio()
 
         self.ask_chatgpt()
 
         self.say(self.messages[-1])
 
-    def transcribe_audio(self, filename="test_recording.mp3"):
-        audio_file = open(filename, "rb")
-        print("sending transcription request")
-        transcript = openai.Audio.transcribe("whisper-1", audio_file)
-        # transcript = {"text":"How are you today?"}
-        print("transcribed:", transcript['text'])
-
-        self.messages.append({"role": "user", "content": transcript['text']})
-
-    def ask_chatgpt(self):
-        print("sending request to gpt-3")
-        r = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=self.messages
-        )
-        print("answer received")
-        for c in r['choices']:
-            self.messages.append(c['message'])
-
-    def stop(self):
-        self.st = 0
+    def record(self):
+        self.button.disconnect()
+        self.button.clicked.connect(self.stop_recording)
+        self.button.setText("Stop recording !")
+        # force update gui
+        QApplication.processEvents()
+        # start recording thread
+        self.recorder = RecordingWorker(format=self.FORMAT, channels=self.CHANNELS,
+                                        rate=self.RATE, chunk=self.CHUNK)
+        # Execute
+        self.threadpool.start(self.recorder)
 
 
-# Create an object of the ProgramGUI class to begin the program.
-guiAUD = RecAUD()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
